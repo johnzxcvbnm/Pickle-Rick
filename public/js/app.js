c49f73ed99b4c057d93b7fbee6475246143471e1
John Kusching <kimshrum@Kims-Air.home>
2018-10-14 12:59:29 -0500

    Added Angular functionality.  Added pages About, Register and Login.  Tested ability to switch pages.

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,15 @@
-const app = angular.module("Css2Go_App", [])
+const app = angular.module("Css2Go_App", []);
 
 app.controller("MainController", ['$http', function($http){
+
+  //Variables
   const controller = this;
+  this.includePath = 'partials/home.html';
+
+  //Functions
+  this.changeInclude = (path) => {
+    this.includePath = 'partials/' + path + '.html';
+  }
 
   
 }])
